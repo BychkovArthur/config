aaf928c17c1f43a1f4fb80bd308c6422a31dca91
BychkovArthur <109972318+BychkovArthur@users.noreply.github.com>
2024-01-23 14:24:05 +0300

    Add files via upload

---
+++ temp.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long int;
+using ull = unsigned long long int;
+using vi = vector<int>;
+using vll = vector<long long int>;
+template <typename _Key, typename _Tp>
+using umap = unordered_map<_Key, _Tp>;
+template <typename _Value>
+using uset = unordered_set<_Value>;
+
+static const ll mod1 = 1'000'000'007;
+static const ll mod2 = 2'147'483'647;
+
+/*
+    TODO: шаблонный swap
+*/
+
+int main() {
+    return 0;
+}
